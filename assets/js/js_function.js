7ca6be494901e1344457d72d5ed699c0567b22a5
yifanguan <yfguan@umich.edu>
2020-07-04 05:16:25 -0400

    change publication format; and add friends random link

---
+++ assets/js/js_function.js
@@ -0,0 +1,15 @@
+var sites = [
+    'https://niceirene.github.io/',
+    'https://web.eecs.umich.edu/~chshibo/',
+    'https://shengpu1126.github.io/',
+    'https://yifanguan.github.io/',
+    'https://xingjunjie.me/blog/',
+    'http://www-personal.umich.edu/~pujat/',
+    'https://pyjhzwh.github.io/',
+    'https://ddshan.github.io/'
+];
+
+function randomSite() {
+    var i = parseInt(Math.random() * sites.length);
+    window.location.href = sites[i];
+}
