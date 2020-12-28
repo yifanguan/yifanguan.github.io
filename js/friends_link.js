824a3b4c37cf052e3326fa73721575493e53637e
yifanguan <yfguan@umich.edu>
2020-12-28 15:51:03 -0500

    ~

--- js/friends_link.js
+++ js/friends_link.js
@@ -1,9 +1,9 @@
 var sites = [
-    'https://niceirene.github.io/',
+    // 'https://niceirene.github.io/',
     'https://web.eecs.umich.edu/~chshibo/',
-    'https://shengpu1126.github.io/',
+    // 'https://shengpu1126.github.io/',
     'https://yifanguan.github.io/',
-    'https://xingjunjie.me/blog/',
+    // 'https://xingjunjie.me/blog/',
     'http://www-personal.umich.edu/~pujat/',
     'https://pyjhzwh.github.io/',
     'https://ddshan.github.io/'
