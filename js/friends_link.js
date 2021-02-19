6598e5d070348f803da53e6bb7c4ec68e1fc6cc3
yifanguan <yfguan@umich.edu>
2021-02-19 01:35:30 -0500

    update friend link

--- js/friends_link.js
+++ js/friends_link.js
@@ -1,12 +1,17 @@
 var sites = [
+    'https://wuwenw.github.io/',
+    'https://jw-liu.xyz/',
+    'https://xzhu27.me/',
+    'https://vic0428.github.io/',
+    'https://waterpine.github.io/'
     // 'https://niceirene.github.io/',
-    'https://web.eecs.umich.edu/~chshibo/',
+    // 'https://web.eecs.umich.edu/~chshibo/',
     // 'https://shengpu1126.github.io/',
-    'https://yifanguan.github.io/',
+    // 'https://yifanguan.github.io/',
     // 'https://xingjunjie.me/blog/',
-    'http://www-personal.umich.edu/~pujat/',
-    'https://pyjhzwh.github.io/',
-    'https://ddshan.github.io/'
+    // 'http://www-personal.umich.edu/~pujat/',
+    // 'https://pyjhzwh.github.io/',
+    // 'https://ddshan.github.io/'
 ];
 
 function randomSite() {
